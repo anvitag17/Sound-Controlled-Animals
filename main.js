64e1735a4c9fb9eafedd8c2e069468b5c610658a
anvitag17 <75227469+anvitag17@users.noreply.github.com>
2021-11-01 12:08:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,12 +19,6 @@ function gotResults(error, results) {
     random_number_g = Math.floor(Math.random() * 255) + 1;
     random_number_b = Math.floor(Math.random() * 255) + 1;
 
-
-    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
-    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
-    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
-    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
-
     img = document.getElementById('animal_image');
 
     if (results[0].label == "Barking") {
